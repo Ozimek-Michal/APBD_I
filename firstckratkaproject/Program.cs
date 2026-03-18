9240d71d3f170007fcbafae7ab44f0bbac383b6c
Michal Ozimek <s33994@pjwstk.edu.pl>
2026-03-18 23:42:48 +0100

    Added perceptron class and classify function

--- firstckratkaproject/Program.cs
+++ firstckratkaproject/Program.cs
@@ -22,5 +22,39 @@ class  Program
             }
         }
         Console.WriteLine(string.Join(", ", vector));
+        Perceptron per = new Perceptron(0.2, 0.2);
+        if (per.classify(vector))
+        {
+            Console.WriteLine("Perceptron activated!");
+        }
+        else
+        {
+            Console.WriteLine("Perceptron not activated!");
+        }
+    }
+}
+
+class Perceptron
+{
+    private double[] weights = { 0.5, 0.2, 0.5, 1 };
+    private double threshold = 4;
+    private double alpha;
+    private double beta;
+
+    public Perceptron(double a, double b)
+    {
+        alpha = a;
+        beta = b;
+    }
+
+    public bool classify(int[] vector)
+    {
+        double scalar = 0;
+        for (int i = 0; i < weights.Length; i++)
+        {
+            scalar += vector.ElementAt(i) * weights.ElementAt(i);
+        }
+        
+        return scalar > threshold;
     }
 }
